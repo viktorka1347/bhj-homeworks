9b6cf0e797ae3fc9fa94e350a98a90d9ac95ec6c
Viktoria Ulyanchenko <viktorka1347@gmail.com>
2021-07-27 18:41:06 +0300

    js-features/cookie-clicker/task.js

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,7 +1,12 @@
-var link = document.getElementById("cookie"),
- counter = document.getElementById("clicker__counter"),
+const link = document.getElementById("cookie"),
+ counter = document.getElementById("clicker__counter");
  count = 0;
-link.onclick = function() {
+ let a = 9/10;
+
+link.onclick = function () {
  count++;
+ link.width *=a;
+ link.height *=a;
+ a = 1/a;
  counter.textContent = count;
-};
+}
